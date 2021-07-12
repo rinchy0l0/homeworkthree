90308bd2eedffda43e52fc86e2d91473cc8b2969
rinchy0l0 <rinchyolo@mail.ru>
2021-07-13 01:38:48 +0300

    My second commit

---
+++ src/main/java/guru/qa/Cat.java
@@ -0,0 +1,38 @@
+package guru.qa;
+
+public class Cat {
+
+    static int numberOfMothers = 1;
+    String coatColor;
+    String name;
+    boolean sex; // false - girl, true -  man
+    int age;
+
+    Cat(String a, String b,  boolean t, int f)
+    {
+        coatColor  = a ;
+        name = b ;
+        sex = t;
+        age = f;
+    }
+    Cat()
+    {
+    }
+
+    Cat(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    void displayInfo(){
+        System.out.printf("Coat color " + coatColor, " cat name " + name, " sex cat " + sex, " age cat" + age);
+    }
+
+    void displayInfoForZhora(){
+        System.out.printf( " cat name " + name, " age cat" + age);
+    }
+    static void sayNumberOfMothers () {
+        System.out.println(numberOfMothers);
+    }
+}
---
+++ src/main/java/guru/qa/Main.java
@@ -0,0 +1,23 @@
+package guru.qa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cat liza = new Cat ("Green", "Liza", false, 6);
+        liza.displayInfo();
+        liza.sayNumberOfMothers();
+
+        Cat tom = new Cat();
+        tom.coatColor = "Blue";
+        tom.name = "Tom";
+        tom.sex = true;
+        tom.age = 1;
+        tom.sayNumberOfMothers();
+        tom.displayInfo();
+
+        Cat zhora = new Cat("Zhora", 2);
+        zhora.displayInfoForZhora();
+        zhora.sayNumberOfMothers();
+
+    }
+}
